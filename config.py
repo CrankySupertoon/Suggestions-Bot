b528c1185c9fdf7e3f86f38a4d9afcb41a1bab9b
Accieo <42718776+Accieo@users.noreply.github.com>
2018-11-07 13:03:49 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+token = ""  # Your bot's token
+reactionEmojis = ["👍","👎"] # Reactions to be added to messages
+channelID = "" # ID of channel in which reactions will be added to messages
---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import config
+from discord.ext import commands
+from discord.utils import get
+
+client = commands.Bot(description = "Adds reactions automatically to suggestions", command_prefix = "")
+
+@client.event
+async def on_ready():
+    global channel
+    print("#################\n# Bot is online #\n#################")
+    print("Running as: " + client.user.name)
+    print("ID: " + client.user.id)
+    print("Discord.py: " + discord.__version__)
+    print("Created by Accieo#8858")
+    channel = client.get_channel(config.channelID)
+
+@client.event
+async def on_message(ctx):
+    if ctx.channel == channel:
+        for emoji in config.reactionEmojis:
+            await client.add_reaction(ctx, emoji)
+
+client.run(config.token)
